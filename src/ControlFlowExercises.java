1ce2466f485904b3dd8b64d9d51aa671fed4cd1c
Horace Smith <horacesmith26@gmail.com>
2023-01-30 15:29:31 -0600

    java second exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,8 @@
 public class ControlFlowExercises {
     public static void main(String[] arg) {
-        long i = 2;
-        do {
-            System.out.println(i);
-            i *= i;
-        } while(i < 1000000 );
+        for(long i = 2; i < 1000000; i *= i) {
+            System.out.println( i);
+        }
 
     }
 }

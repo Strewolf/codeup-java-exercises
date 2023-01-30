f991cafda5158280bc50ec8bd3cdbcde6dc8b6d5
Horace Smith <horacesmith26@gmail.com>
2023-01-30 12:09:05 -0600

    java second exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        int myFavoriteNumber = 25;
+        float myNumber= (float)3.14f ;
+        String myString = "hey";
+        System.out.println(myFavoriteNumber);
+        System.out.println(myString);
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(y);
     }
 }

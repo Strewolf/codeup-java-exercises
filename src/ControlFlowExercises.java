988e6a00be58955bf51df5ca43c9068767230d39
Horace Smith <horacesmith26@gmail.com>
2023-01-30 15:02:43 -0600

    java second exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,10 @@
 public class ControlFlowExercises {
     public static void main(String[] arg) {
-        int i = 0;
+        int i = 100;
         do {
             System.out.print(i); System.out.append(" ");
-            i += 2;
-        }while(i<=100 );
+            i -= 5;
+        }while(i>=-10 );
 
     }
 }

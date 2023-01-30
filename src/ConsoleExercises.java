2f7a06f8c3de4f73ad571d824e25bbd786b68a1f
Horace Smith <horacesmith26@gmail.com>
2023-01-30 13:47:31 -0600

    java second exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -24,12 +24,18 @@ public class ConsoleExercises {
         System.out.print("Please enter the width of the classroom: ");
         int width = scanner.nextInt();
 
+        System.out.print("Please enter the width of the classroom: ");
+        int height = scanner.nextInt();
+
         int area = length * width;
         int perimeter = (length * 2) + (width * 2);
+        int volume = length * width * height;
 
         System.out.println("The area of the classroom is: " + area);
 
-        System.out.println("The area of the classroom is: " + perimeter);
+        System.out.println("The perimeter of the classroom is: " + perimeter);
+
+        System.out.println("The volume of the classroom is:" + volume);
     }
 }
 

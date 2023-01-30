17382c9160505c05ed747cd7942238c8969eee7b
Horace Smith <horacesmith26@gmail.com>
2023-01-30 13:38:18 -0600

    java second exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,20 +1,38 @@
 import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args){
-        double pi = 3.14159;
-        System.out.format("The value of pi is approximately %.2f%n",pi);
+//        double pi = 3.14159;
+//        System.out.format("The value of pi is approximately %.2f%n",pi);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("please enter 3 Sentences: ");
+//        String userInput = scanner.nextLine(), userInput1  = scanner.nextLine(), userInput2 = scanner.nextLine();
+//
+//
+//
+//        System.out.println("You entered: --> \""+ userInput + "\" <--");
+//        System.out.println("You entered: --> \""+ userInput1 + "\" <--");
+//        System.out.println("You entered: --> \""+ userInput2 + "\" <--");
+//        import java.util.Scanner;
+//
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("please enter 3 words: ");
-        String userInput = scanner.next(), userInput1  = scanner.next(), userInput2 = scanner.next();
+        System.out.print("Please enter the length of the classroom: ");
+        int length = scanner.nextInt();
 
+        System.out.print("Please enter the width of the classroom: ");
+        int width = scanner.nextInt();
 
+        int area = length * width;
+        int perimeter = (length * 2) + (width * 2);
 
-        System.out.println("You entered: --> \""+ userInput + "\" <--");
-        System.out.println("You entered: --> \""+ userInput1 + "\" <--");
-        System.out.println("You entered: --> \""+ userInput2 + "\" <--");
+        System.out.println("The area of the classroom is: " + area);
 
-
-
+        System.out.println("The area of the classroom is: " + perimeter);
     }
 }
+
+
+
+

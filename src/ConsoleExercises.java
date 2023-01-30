c1eecd59a262d9c012e3179fadfd253ff0471fb3
Horace Smith <horacesmith26@gmail.com>
2023-01-30 12:44:04 -0600

    java second exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args){
         double pi = 3.14159;
         System.out.format("The value of pi is approximately %.2f%n",pi);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("please enter 3 words: ");
+        String userInput = scanner.next(), userInput1  = scanner.next(), userInput2 = scanner.next();
+
+
+
+        System.out.println("You entered: --> \""+ userInput + "\" <--");
+        System.out.println("You entered: --> \""+ userInput1 + "\" <--");
+        System.out.println("You entered: --> \""+ userInput2 + "\" <--");
+
+
 
     }
 }

1ef7059ef180815259030fdb9bb319238848f03b
Horace Smith <horacesmith26@gmail.com>
2023-01-31 14:11:18 -0600

    java loop exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -11,11 +11,17 @@ public class ControlFlowExercises {
         System.out.println("number | squared | Cubed");
         System.out.println("------ | ------- | -----");
 
+//        for (int i = 1; i <= number; i++) {
+//            if (i == number + 1) {
+//                break;
+//            }
+//            System.out.println(i + "      | " + (int)Math.pow(i, 2)+ "       | " + (int)Math.pow(i, 3) );
+//        }
         for (int i = 1; i <= number; i++) {
             if (i == number + 1) {
                 break;
             }
-            System.out.println(i + "      | " + (int)Math.pow(i, 2)+ "       | " + (int)Math.pow(i, 3) );
+            System.out.println(i + "      | " + (int)Math.pow(i, 2) + "       | " + (int)Math.pow(i, 3));
         }
 
 //        int squared = number;

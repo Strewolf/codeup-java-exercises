66893a5d1a229671d284043bf5fc044a65fabb8c
Horace Smith <horacesmith26@gmail.com>
2023-01-31 14:43:27 -0600

    java loop exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,32 +3,31 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] arg) {
         Scanner scanner = new Scanner(System.in);
+        String userInput = "y";
 
-        System.out.print("What number would you like to go up to?");
-        int number = scanner.nextInt();
-
-        System.out.println("Here is your table!");
-        System.out.println("number | squared | Cubed");
-        System.out.println("------ | ------- | -----");
-
-//        for (int i = 1; i <= number; i++) {
-//            if (i == number + 1) {
-//                break;
-//            }
-//            System.out.println(i + "      | " + (int)Math.pow(i, 2)+ "       | " + (int)Math.pow(i, 3) );
-//        }
-        for (int i = 1; i <= number; i++) {
-            if (i == number + 1) {
-                break;
-            }
-            System.out.println(i + "      | " + (int)Math.pow(i, 2) + "       | " + (int)Math.pow(i, 3));
+        while (userInput.equalsIgnoreCase("y")) {
+        System.out.println("Please enter your numerical grade ranging from 0 to 100:");
+        int grade = scanner.nextInt();
+        if(grade >= 88 && grade <= 100){
+            System.out.println(" Your letter grade is : A");
+        }
+         else if (grade >= 80 && grade <= 87) {
+            System.out.println(" Your letter grade is : B");
+        }
+        else if (grade >= 67 && grade <= 79) {
+            System.out.println(" Your letter grade is : C");
+        }
+        else if (grade >= 60 && grade <= 66) {
+            System.out.println(" Your letter grade is : D");
+        }
+        else if (grade >= 0 && grade <= 59) {
+            System.out.println(" Your letter grade is : F");
         }
 
-//        int squared = number;
-//        for (int i = 1; i <= number; i *= ) {
-//            if (i == number + 1) {
-//                break;
-//            }
-//            System.out.println(i);
-//        }
-    }}
+            System.out.println("Would you like to continue? [y/n]: ");
+            String answer = scanner.next();
+            if (answer.equalsIgnoreCase("n")) {
+                System.out.println("Thank you for using smith grading services");
+            }
+
+    }}}

54a4acf6ebc3f882ec294f3f9bb1437ecdc9e26a
Horace Smith <horacesmith26@gmail.com>
2023-02-13 22:45:42 -0600

    java 2 oop exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,9 +1,88 @@
+//import java.util.Arrays;
+//
+//public class ArraysExercises {
+//    private String name;
+//
+//    class Person {
+//        private String name;
+//
+//        public Person (String name){
+//            this.name = name;
+//    }
+//
+//        public String getName() {
+//            return name;
+//        }
+//
+//
+//        public void setName(String name) {
+//            this.name = name;
+//        }
+//
+//        public static Person[] addPerson(Person[] people, Person newPerson ){
+//       Person[] newArray = new Person[people.length + 1];
+//       for (int i = 0; i< people.length; i++) {
+//            newArray[i] = people[i];
+//       }
+//        newArray[people.length] =  newPerson;
+//       return newArray;
+//
+//    }
+//    public static void main(String[] args) {
+//    int[] numbers = {1,2,3,4,5};
+//        System.out.println(Arrays.toString(numbers));
+//        Person [] people = new Person[3];
+//        people[0] = new Person("john");
+//        people[1] = new Person("tim");
+//        People[2] = new Person("tom");
+//        for (Person person : people){
+//            System.out.println(person.getName());
+//        }
+//        Person[] newPerson = new Person("Horace");
+//        people = addPerson(people, newPerson);
+//        System.out.println("after adding new object array" + Arrays.toString(people));
+//
+//
+//}}}
 import java.util.Arrays;
 
 public class ArraysExercises {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(numbers));
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(Arrays.toString(numbers));
 
+        Person[] people = new Person[3];
+        people[0] = new Person("John");
+        people[1] = new Person("Jane");
+        people[2] = new Person("Jim");
+
+        for (Person person : people) {
+            System.out.println(person.getName());
+        }
+
+        Person newPerson = new Person("Joan");
+        people = addPerson(people, newPerson);
+        for (Person person : people){
+            System.out.println("After adding a new person: " + person.getName());
+        }
+
+    }
+
+    public static Person[] addPerson(Person[] people, Person newPerson) {
+        Person[] newArray = Arrays.copyOf(people, people.length + 1);
+        newArray[people.length] = newPerson;
+        return newArray;
+    }
+}
+
+class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,41 +1,41 @@
-public class Person {
-    private String name;
-    public Person(String name){
-        this.name = name;
-    };
-
-    public String getName () {
-//TODO: return the person's name
-        return this.name;
-
-    }
-
-    public void setName (String name){
-//TODO: change the name field to the passed value
-        this.name = name;
-
-    }
-    public void sayHello () {
-//TODO: print a message to the console using the person's name
-        System.out.println("Hello to "+ this.name);
-    }
-
-
-    public static void main(String[] args) {
-// Person Person = new Person("Horace Smith");
-//        System.out.println(Person.getName());
-//Person.sayHello();
-//        Person.setName("joe");
-//        Person.sayHello();
-        Person person1 = new Person("John");
-        Person person2 = person1;
-        System.out.println(person1.getName());
-        System.out.println(person2.getName());
-        person2.setName("Jane");
-        System.out.println(person1.getName());
-        System.out.println(person2.getName());
-
-
-
-    }
-}
+//public class Person {
+//    private String name;
+//    public Person(String name){
+//        this.name = name;
+//    };
+//
+//    public String getName () {
+////TODO: return the person's name
+//        return this.name;
+//
+//    }
+//
+//    public void setName (String name){
+////TODO: change the name field to the passed value
+//        this.name = name;
+//
+//    }
+//    public void sayHello () {
+////TODO: print a message to the console using the person's name
+//        System.out.println("Hello to "+ this.name);
+//    }
+//
+//
+//    public static void main(String[] args) {
+//// Person Person = new Person("Horace Smith");
+////        System.out.println(Person.getName());
+////Person.sayHello();
+////        Person.setName("joe");
+////        Person.sayHello();
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//
+//
+//
+//    }
+//}

34da2b0c62ed4169270961e92870470950d15a6c
Horace Smith <horacesmith26@gmail.com>
2023-02-13 22:48:35 -0600

    java 2 array exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -48,8 +48,6 @@ import java.util.Arrays;
 
 public class ArraysExercises {
     public static void main(String[] args) {
-//        int[] numbers = {1, 2, 3, 4, 5};
-//        System.out.println(Arrays.toString(numbers));
 
         Person[] people = new Person[3];
         people[0] = new Person("John");

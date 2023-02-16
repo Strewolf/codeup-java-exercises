d271bec9a1d593ff3b82056e982a32eb31fee667
Horace Smith <horacesmith26@gmail.com>
2023-02-16 11:30:23 -0600

    java 2 array exercise

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,4 +1,18 @@
 package shapes;
 
-public class Quadrilateral {
+abstract class Quadrilateral extends Shape implements Measurable {
+    protected double length;
+    protected double width;
+
+    public Quadrilateral(double length, double width){
+       this.length = length;
+       this.width = width;
+    }
+    public void setLength(){
+        return this.length = length;
+    }
+    public void setWidth(){
+        return this.width = width;
+    }
+
 }

0a0e8d223cd77f40c28b30e832d45e038de163cd
Horace Smith <horacesmith26@gmail.com>
2023-02-16 10:58:48 -0600

    java 2 array exercise

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,17 +1,17 @@
 package shapes;
 
 public class Square extends Rectangle {
- public Square(double side){
-     super(side,side);
- }
-
- public double getArea(){
-     double area = Math.pow( length, 2);
-     return area;
- }
-
- public double getPerimeter(){
-     double perimeter = length * 4;
-     return perimeter;
- }
+// public Square(double side){
+//     super(side,side);
+// }
+//
+// public double getArea(){
+//     double area = Math.pow( length, 2);
+//     return area;
+// }
+//
+// public double getPerimeter(){
+//     double perimeter = length * 4;
+//     return perimeter;
+// }
 }
